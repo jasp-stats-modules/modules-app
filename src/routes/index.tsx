382ec52ace5e221efabebe5f79b650714e48ea8b
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-08-05 18:00:32 +0200

    Add crude page to show contents of scraped data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,70 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { channels, assets } from '../index.json'
+import type { Releases, Repos, SubModule } from "@/types";
+
+const channels2repos = channels as unknown as Repos
+const releaseAssets = assets as unknown as Releases;
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
+function Repo({repo}: {repo: SubModule}) {
+  const shortDescriptionHTML = releaseAssets[repo.nameWithOwner]?.shortDescriptionHTML || '';
+  const latestRelease = releaseAssets[repo.nameWithOwner]?.releases?.find(r => r.isLatest);
+
+  return (
+    <details>
+      <summary>{repo.nameWithOwner}</summary>
+      <p>{shortDescriptionHTML}</p>
+      {latestRelease && (
+        <div>
+          <h4>Latest Release: {latestRelease.name}</h4>
+          <p>Published at: {latestRelease.publishedAt}</p>
+          <ul>
+            {latestRelease.releaseAssets.nodes.map(asset => (
+              <li key={asset.downloadUrl}>
+                <a href={asset.downloadUrl} target="_blank" rel="noopener noreferrer">
+                  {asset.name} ({asset.downloadCount} downloads)
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <hr />
+    </details>
+  )
+}
+
+function Channel({ channel}: { channel: string }) {
+  const repos = channels2repos[channel] || [];
+  return (
+    <details open>
+      <summary>{channel}</summary>
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.nameWithOwner}>
+             <Repo repo={repo} />
+          </li>
+        ))}
+      </ul>
+    </details>
+  )
+
+}
+
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div>
+      <h1>JASP Modules</h1>
+      <ul>
+        {Object.keys(channels2repos).map(channel => (
+          <li key={channel}>
+            <Channel channel={channel} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
