1bf6479407b607339fe32f849ef81e07602fe4b5
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2026-01-27 09:12:47 +0100

    Make muted text on card bg with light theme have more constrast

--- src/App.tsx
+++ src/App.tsx
@@ -353,7 +353,7 @@ function RepositoryChannels({
           key={channel}
           className={cn(
             'inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden whitespace-nowrap rounded-full border px-2 py-0.5 font-medium text-sm transition-[color,box-shadow] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&>svg]:pointer-events-none [&>svg]:size-3',
-            'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+            'border-transparent bg-background text-secondary-foreground [a&]:hover:bg-background/90',
           )}
           title={channelText.value}
         >
